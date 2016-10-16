db21f2e5a1bf533f457e1e578c3a20c9142f4694
Simon Peter Campbell <storm20200@gmail.com>
2016-10-16 02:18:55 +0100

    Automatically compile GLFW if possible on Linux.

---
+++ scripts/build-glfw-linux.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Unfortunately dub doesn't always run the script from the correct working directory, as such we need to cd to the
+# current script directory and return to the previous upon exit.
+currentDir=`pwd`
+shDir="${0%/*}"
+shFileName="${0##*/}"
+
+echo ""
+echo "================================="
+echo "Starting $shFileName..."
+echo "================================="
+echo "" 
+
+cd $shDir
+
+arch="$1"
+folder="linux-$arch"
+glfw="../external/glfw"
+output="../content/$folder"
+extraCMakeFlags=""
+
+exit_script ()
+{
+    echo ""
+    echo "================================="
+    echo "Exiting $shFileName..."
+    echo "================================="
+    echo "" 
+    exit 0
+}
+
+if [ "$arch" == "x86" ]; then
+    extraCMakeFlags="-DCMAKE_C_COMPILER_ARG1=-m32"
+elif [ "$arch" != "x86_64" ]; then
+    echo "Invalid Arch paramter given: $arch."
+    exit_script
+fi;
+
+if [ ! -e "$glfw/CMakeLists.txt" ]; then
+    echo "Couldn't find external/glfw/CMakeLists.txt, makes sure you run 'git submodule update --init --recursive'"
+    exit_script
+fi;
+
+mkdir -p $output
+mkdir -p ../.temp/$folder
+cd ../.temp/$folder
+
+echo "Running CMake on GLFW..."
+cmake ../$glfw -DBUILD_SHARED_LIBS=ON -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF -DCMAKE_BUILD_TYPE=Release $extraCMakeFlags > /dev/null
+echo ""
+
+echo "Attempting to build GLFW..."
+cmake --build . --target glfw > /dev/null
+echo ""
+
+echo "Copying libglfw.so for $folder..."
+cp src/libglfw.so ../$output > /dev/null
+
+cd $currentDir
+
+exit_script
--- scripts/build-glfw-windows.bat
+++ scripts/build-glfw-windows.bat
@@ -3,14 +3,7 @@ REM Unfortunately dub doesn't always run the script from the correct directory. 
 REM directory storing the batch file.
 set currentDir=%cd%
 set batchDir=%~dp0
-cd %batchDir%
-
 set batchFileName=%~n0
-set arch=%1
-set folder=win%arch%
-set glfw=..\external\glfw
-set output=..\content\%folder%
-set generator=""
 
 echo: 
 echo:=================================
@@ -18,21 +11,29 @@ echo:Starting %batchFileName%...
 echo:=================================
 echo: 
 
+cd %batchDir%
+
+set arch=%1
+set folder=win%arch%
+set glfw=..\external\glfw
+set output=..\content\%folder%
+set generator=""
+
 if %arch% == 32 (
     set generator="Visual Studio 14 2015"
 ) else if %arch% == 64 (
     set generator="Visual Studio 14 2015 Win64"
 ) else (
-    echo:Incorrect Arch parameter given: %arch%
+    echo:Incorrect Arch parameter given: %arch%.
     cd %currentDir%
     exit /B 0
 )
 
-if not exist ..\temp mkdir ..\temp
-if not exist ..\temp\%folder% mkdir ..\temp\%folder%
+if not exist ..\.temp mkdir ..\.temp
+if not exist ..\.temp\%folder% mkdir ..\.temp\%folder%
 if not exist %output% mkdir %output%
 
-cd ..\temp\%folder%
+cd ..\.temp\%folder%
 
 echo:Running CMake on GLFW with generator: %generator%...
 cmake ..\%glfw% -DBUILD_SHARED_LIBS=ON -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF -G %generator% > nul
@@ -50,6 +51,6 @@ cd %currentDir%
 
 echo: 
 echo:=================================
-echo:%batchFileName% finished...
+echo:Exiting %batchFileName%...
 echo:=================================
 echo: 
