4bb1ebf9888b82c7e1e840e10996e40fc34360eb
Simon Peter Campbell <storm20200@gmail.com>
2016-11-01 05:28:26 +0000

    Implemented some metaprogramming utility functions for use by other parts of the engine.

---
+++ Source/denjin/utility/package.d
@@ -0,0 +1,9 @@
+/**
+    Contains general purpose utility functions used throughout Denjin.
+
+    Authors: Simon Peter Campbell, peter@spcampbell.co.uk
+    Copyright: MIT
+*/
+module denjin.utility;
+
+public import denjin.utility.meta;
