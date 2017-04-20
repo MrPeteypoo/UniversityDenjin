bcb3743a1c08eeeceb0f6b5fc152e8928f3db69d
storm20200 <peter@spcampbell.co.uk>
2017-04-20 03:34:58 +0100

    Fixed Travis CI outputting success all the time?

--- build.sh
+++ build.sh
@@ -24,8 +24,8 @@ if [ "$compiler" != "" ]; then
     compilerFlag="--compiler=$compiler"
 fi
 
-dub $command $buildFlag $archFlag $compilerFlag
-dub $command denjin:maths $buildFlag $archFlag $compilerFlag
-dub $command denjin:misc $buildFlag $archFlag $compilerFlag
-dub $command denjin:rendering $buildFlag $archFlag $compilerFlag
-dub $command denjin:window $buildFlag $archFlag $compilerFlag
+dub $command $buildFlag $archFlag $compilerFlag || exit $?
+dub $command denjin:maths $buildFlag $archFlag $compilerFlag || exit $?
+dub $command denjin:misc $buildFlag $archFlag $compilerFlag || exit $?
+dub $command denjin:rendering $buildFlag $archFlag $compilerFlag || exit $?
+dub $command denjin:window $buildFlag $archFlag $compilerFlag || exit $?
