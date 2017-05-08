dce9126b6adc852fe574bc4b3fcaab1715531758
storm20200 <peter@spcampbell.co.uk>
2017-05-08 21:03:13 +0100

    Another debug tweak

--- build.sh
+++ build.sh
@@ -25,7 +25,7 @@ if [ "$compiler" != "" ]; then
 fi
 
 dub $command $buildFlag $archFlag $compilerFlag --quiet || exit $?
-dub $command denjin:assets $buildFlag $archFlag $compilerFlag --quiet || exit $?
+dub $command denjin:assets $buildFlag $archFlag $compilerFlag || exit $?
 dub $command denjin:maths $buildFlag $archFlag $compilerFlag --quiet || exit $?
 dub $command denjin:misc $buildFlag $archFlag $compilerFlag --quiet || exit $?
 dub $command denjin:rendering $buildFlag $archFlag $compilerFlag --quiet || exit $?
